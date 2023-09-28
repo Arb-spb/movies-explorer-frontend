e78df42c095dcc9934a69b480f2848e8b6fb504e
Арбачаков Валерий <valeriy.arbachakov@gazprombank.ru>
2023-09-28 21:59:15 +0300

    #6 feat: Вёрстка - create page Login and Register (https://github.com/Arb-spb/movies-explorer-frontend/issues/6)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,43 @@
+import './Login.css';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.svg';
+import { ROUTE_REGISTER } from '../../constants';
+
 function Login() {
   return (
-    <div>
-      <div>Login</div>
+    <div className="Login">
+      <div className="Login__container">
+        <div className="Login__logo">
+          <img src={logo} alt="логотип" />
+        </div>
+        <h3 className="Login__title">Рады видеть!</h3>
+        <form className="Login__from">
+          <div className="Login__group">
+            <lable htmlFor="email" className="Login__label">E-mail</lable>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="Login__input"
+              placeholder=""
+            />
+          </div>
+          <div>
+            <lable htmlFor="password" className="Login__label">Пароль</lable>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              className="Login__input"
+              placeholder=""
+            />
+          </div>
+          <button type="submit" className="Login__submit">
+            <span>Войти</span>
+          </button>
+        </form>
+        <p className="Login__text">Ещё не&nbsp;зарегистрированы? <Link to={ROUTE_REGISTER} className="Login__register">Регистрация</Link></p>
+      </div>
     </div>
   )
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,8 +1,54 @@
+import './Register.css';
+import logo from "../../images/logo.svg";
+import {Link} from "react-router-dom";
+import {ROUTE_REGISTER} from "../../constants";
+
 function Register() {
   return (
-    <div>
-      <div>Register</div>
-    </div>
+    <section className="Register">
+      <div className="Register__container">
+        <div className="Register__logo">
+          <img src={logo} alt="логотип" />
+        </div>
+        <h3 className="Register__title">Добро пожаловать!</h3>
+        <form className="Register__from">
+          <div className="Register__group">
+            <lable htmlFor="name" className="Register__label">Имя</lable>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              className="Register__input"
+              placeholder=""
+            />
+          </div>
+          <div className="Register__group">
+            <lable htmlFor="email" className="Register__label">E-mail</lable>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              className="Register__input"
+              placeholder=""
+            />
+          </div>
+          <div className="Register__group">
+            <lable htmlFor="password" className="Register__label">E-mail</lable>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              className="Register__input"
+              placeholder=""
+            />
+          </div>
+          <button type="submit" className="Register__submit">
+            <span>Зарегистрироваться</span>
+          </button>
+        </form>
+        <p className="Register__text">Уже зарегистрированы? <Link to={ROUTE_REGISTER} className="Register__register">Войти</Link></p>
+      </div>
+    </section>
   )
 }
 
